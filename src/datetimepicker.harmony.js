3e45cd18caa2952f8b04adfdbfa39481008e55fa
liu-bug123 <@1518171514@qq.com>
2023-12-18 18:58:03 +0800

    初次剔除无用文件

--- src/datetimepicker.harmony.js
+++ src/datetimepicker.harmony.js
@@ -1,13 +1,25 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * MIT License
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  *
- * This is a controlled component version of RNDateTimePicker
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * @format
- * @flow strict-local
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 import RNDateTimePicker from './picker';
 import {dateToMilliseconds, sharedPropsValidation} from './utils';
@@ -18,7 +30,7 @@ import {
   IOS_MODE,
 } from './constants';
 import * as React from 'react';
-import {Platform} from 'react-native';
+//import {Platform} from 'react-native';
 
 import type {
   DateTimePickerEvent,
@@ -27,21 +39,21 @@ import type {
   IOSDisplay,
 } from './types';
 
-const getDisplaySafe = (display: IOSDisplay): IOSDisplay => {
-  const majorVersionIOS = parseInt(Platform.Version, 10);
-  if (display === IOS_DISPLAY.inline && majorVersionIOS < 14) {
-    // inline is available since 14.0
-    return IOS_DISPLAY.spinner;
-  }
-  if (majorVersionIOS < 14) {
-    // NOTE this should compare against 13.4, not 14 according to https://developer.apple.com/documentation/uikit/uidatepickerstyle/uidatepickerstylecompact?changes=latest_minor&language=objc
-    // but UIDatePickerStyleCompact does not seem to work prior to 14
-    // only the spinner display (UIDatePickerStyleWheels) is thus available below 14
-    return IOS_DISPLAY.spinner;
-  }
-
-  return display;
-};
+// const getDisplaySafe = (display: IOSDisplay): IOSDisplay=>{
+//     const majorVersionIOS = parseInt(Platfrom.Version, 10);
+//     if(display === IOS_DISPLAY.inline && majorVersionIOS < 14){
+//         // inline is available since 14.0
+//         return IOS_DISPLAY.spinner;
+//     }
+//     if(majorVersionIOS < 14){
+//         // NOTE this should compare against 13.4, not 14 according to https://developer.apple.com/documentation/uikit/uidatepickerstyle/uidatepickerstylecompact?changes=latest_minor&language=objc
+//         // but UIDatePickerStyleCompact does not seem to work prior to 14
+//         // only the spinner display (UIDatePickerStyleWheels) is thus available below 14
+//         return IOS_DISPLAY.spinner;
+//     }
+//
+//     return display
+// }
 
 export default function Picker({
   value,
@@ -58,11 +70,13 @@ export default function Picker({
   mode = IOS_MODE.date,
   display: providedDisplay = IOS_DISPLAY.default,
   disabled = false,
+  is24Hour = true,
   ...other
 }: IOSNativeProps): React.Node {
   sharedPropsValidation({value, timeZoneOffsetInMinutes, timeZoneName});
 
-  const display = getDisplaySafe(providedDisplay);
+  // const display = getDisplaySafe(providedDisplay)
+  const display = providedDisplay;
 
   const _onChange = (event: NativeEventIOS) => {
     const timestamp = event.nativeEvent.timestamp;
@@ -111,6 +125,7 @@ export default function Picker({
       onResponderTerminationRequest={() => false}
       displayIOS={display}
       enabled={disabled !== true}
+      is24Hour={is24Hour}
     />
   );
 }
